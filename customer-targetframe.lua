a4205e093fa7f95dee0a66e5a27588aa6dd2e576
Sverre Skuland <Sverre Skuland>
2017-01-22 13:18:58 +0100

    Added weak aura target frame

---
+++ customer-targetframe.lua
@@ -0,0 +1,29 @@
+-- Enable "All triggers"
+-- Add as Trigger 2
+-- Type = Custom
+-- Event type = Event
+-- Event(s) = PLAYER_ENTERING_WORLD
+
+-- Custom trigger
+function (e)
+    local r = WeakAuras.regions["Target health"].region
+    local b = CreateFrame("Button", "WATargetFrameButton", r, "SecureUnitButtonTemplate")
+    
+    b:SetAllPoints()
+    b:SetScript("OnMouseDown", function(self, button)
+            
+            if button == "RightButton" then
+                
+                ToggleDropDownMenu(1, nil, TargetFrameDropDown, self, 0, 0)
+                
+            end
+            
+    end)
+    
+    return true
+end
+
+-- Custom untrigger
+function (e)
+	return false
+end
